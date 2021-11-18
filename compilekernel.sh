82f0321c5b3596da571646d127385a57981bc6f2
Navin136 <nkwhitehat@gmail.com>
2021-11-18 12:12:42 +0530

    compilekernel: Update Script
    Signed-off-by: Navin136 <nkwhitehat@gmail.com>

--- compilekernel.sh
+++ compilekernel.sh
@@ -1,43 +1,75 @@
 #!bin/bash
-sudo apt install make bison bc libncurses5-dev git tmate python3-pip curl build-essential zip unzip
-pip install telegram-upload
-if [ -d $HOME/Anykernel ]
+
+# Install Required Packages
+sudo apt install make bison bc libncurses5-dev tmate git python3-pip curl build-essential zip unzip -y
+
+#Working Directory
+WORK_DIR=$(pwd)
+
+# Telegram Chat Id
+ID="-1001509079419"
+
+# Bot Token
+bottoken=$token
+
+# Functions
+msg() {
+	curl -X POST "https://api.telegram.org/bot$bottoken/sendMessage" -d chat_id="$ID" \
+	-d "disable_web_page_preview=true" \
+	-d "parse_mode=html" \
+	-d text="$1"
+}
+file() {
+	MD5=$(md5sum "$1" | cut -d' ' -f1)
+	curl --progress-bar -F document=@"$1" "https://api.telegram.org/bot$bottoken/sendDocument" \
+	-F chat_id="$ID"  \
+	-F "disable_web_page_preview=true" \
+	-F "parse_mode=html" \
+	-F caption="$2 | <b>MD5 Checksum : </b><code>$MD5</code>"
+}
+
+#cloning
+if [ -d $WORK_DIR/Anykernel ]
 then
-	echo "Anykernel Directory Already Exists"
+echo "Anykernel Directory Already Exists"
 else
-git clone --depth=1 https://github.com/nktn30/AnyKernel3 $HOME/Anykernel
+git clone --depth=1 https://github.com/navin136/AnyKernel3 $WORK_DIR/Anykernel
 fi
-if [ -d $HOME/kernel ]
+if [ -d $WORK_DIR/kernel ]
 then
 echo "kernel dir exists"
 else
-git clone --depth=1 https://github.com/nktn30/velocity $HOME/kernel
+git clone --depth=1 https://github.com/navin136/android_kernel_asus_X00TD $WORK_DIR/kernel
 fi
-if [ -d $HOME/toolchains/gcc64 ] && [ -d $HOME/toolchains/gcc32 ]
+if [ -d $WORK_DIR/toolchains/gcc64 ] && [ -d $WORK_DIR/toolchains/gcc32 ]
 then
 echo "gcc dir exists"
 else
-git clone --depth=1 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 -b android11-release $HOME/toolchains/gcc64
-git clone --depth=1 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 -b android11-release $HOME/toolchains/gcc32
+git clone --depth=1 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 -b android12-release $WORK_DIR/toolchains/gcc64
+git clone --depth=1 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 -b android12-release $WORK_DIR/toolchains/gcc32
 fi
-if [ -d $HOME/toolchains/clang ]
+if [ -d $WORK_DIR/toolchains/clang ]
 then
 echo "clang dir exists"
 else
-cd $HOME/toolchains
+cd $WORK_DIR/toolchains
 mkdir clang
 cd clang
-wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/android11-release/clang-r383902b.tar.gz
-tar -xvzf clang-r383902b.tar.gz
+wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/android12-release/clang-r416183b1.tar.gz
+tar -xvzf clang-r416183b1.tar.gz
 fi
-cd $HOME/kernel
+
 #Starting Compilation
+msg "<b>Build Started</b>"
+BUILD_START=$(date +"%s")
+cd $WORK_DIR/kernel
 export ARCH=arm64
 export SUBARCH=arm64
 DATE=$(date +%d'-'%m'-'%y'-'%R)
-K_DIR="$HOME/kernel"
-export PATH="$HOME/toolchains/gcc64/bin/:$HOME/toolchains/gcc32/bin/:$HOME/toolchains/clang/bin/:$PATH"
-cd $HOME/kernel
+K_DIR="$WORK_DIR/kernel"
+export PATH="$WORK_DIR/toolchains/gcc64/bin/:$WORK_DIR/toolchains/gcc32/bin/:$WORK_DIR/toolchains/clang/bin/:$PATH"
+cd $WORK_DIR/kernel
+make clean && make mrproper
 rm -f out/arch/arm64/boot/Image.gz-dtb
 make O=out X00TD_defconfig
 make -j$(nproc --all) O=out \
@@ -45,16 +77,23 @@ make -j$(nproc --all) O=out \
       CROSS_COMPILE=aarch64-linux-android- \
       CROSS_COMPILE_ARM32=arm-linux-androideabi- \
       CC=clang
+
+#Zipping Into Flashable Zip
 if [ -f out/arch/arm64/boot/Image.gz-dtb ]
 then
-cp out/arch/arm64/boot/Image.gz-dtb $HOME/Anykernel
-cd $HOME/Anykernel
-zip -r9 VELOCITY_V1.0-"$DATE".zip * -x .git README.md */placeholder
-cp $HOME/Anykernel/VELOCITY_V1.0-"$DATE".zip $HOME/
-rm $HOME/Anykernel/Image.gz-dtb
-rm $HOME/Anykernel/VELOCITY_V1.0-"$DATE".zip
+cp out/arch/arm64/boot/Image.gz-dtb $WORK_DIR/Anykernel
+cd $WORK_DIR/Anykernel
+zip -r9 VELOCITY-$DATE.zip * -x .git README.md */placeholder
+cp $WORK_DIR/Anykernel/VELOCITY-$DATE.zip $WORK_DIR/
+rm $WORK_DIR/Anykernel/Image.gz-dtb
+rm $WORK_DIR/Anykernel/VELOCITY-$DATE.zip
+BUILD_END=$(date +"%s")
+DIFF=$((BUILD_END - BUILD_START))
+
 #Upload Kernel
-telegram-upload $HOME/VELOCITY_V1.0-"$DATE".zip
+
+file "$WORK_DIR/VELOCITY-$DATE.zip" "Build took : $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)"
+
 else
-	echo "Build Errored!"
+msg "<b>Build Errored!</b>"
 fi
